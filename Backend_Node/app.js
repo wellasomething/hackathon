388875f6dcb5bb5da6995ea7fa07e3ad7d99b165
Samuel <swella000@gmail.com>
2023-04-08 20:22:09 +0100

    n

--- Backend_Node/app.js
+++ Backend_Node/app.js
@@ -27,57 +27,10 @@ app.post("/", (req, res) => {
 });
 
 
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const jwt = require('jsonwebtoken');
-// const app = express();
-// const cors = require('cors');
-
-// app.use(cors());
-// app.use(bodyParser.json());
-
-
-
-// // Regular expression for email validation
-// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-//  // Regular expression for password validation
-// const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>/?]).{8,}$/;
-
-// app.post('/login', (req, res) => {
-//   const { email, password } = req.body;
-  
-
-//    // Validate the email format
-//    if (!emailRegex.test(email)) {
-//     res.status(400).send('Invalid email format');
-//     return;
-//   };
-
-//     // Validate the password format
-//     if (!passwordRegex.test(password)) {
-//       res.status(400).send('Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, 1 special character, and be at least 8 characters long');
-//       return;
-//     }
-
-//     // var emailPass = [{mail:"bimpe@email.com", "pass: "Password123$"},]
-//   // Authenticate the user
-//   if (email === 'bimpe@email.com' && password === 'Password123$') {
-//     const token = jwt.sign({ email }, 'secret_key');
-//     res.send({ token, message:"Welcome" });
-//   } else {
-//     res.status(404).send('Invalid credentials');
-    
-//   }
-// });
-
 //calculating the total amount saved by a user
 app.post('/accomodation', (req, res)=>{
   const inputValue = req.body.inputValue;
-  const totalBalance = 100;
+  const totalBalance = req.body.balance;
 
   //performing percentage computation
   const amountSaved = (inputValue/100) * totalBalance;
@@ -99,6 +52,7 @@ app.post('/accomodation', (req, res)=>{
 const startDate = new Date('2023-04-10'); // Example start date: April 10, 2023
 const endDate = new Date('2023-04-20');
 
+//check the frequency, date money should be deducted
 function scheduleDeduction() {
 
   // Get current date
